26b3f394900489799f59d54edf92616a47cd7e6b
Tobi <contact@iamtobi.eu>
2016-06-16 16:55:26 +0200

    code/folder structure created, first level created, levelmanager implemented

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour
+{
+
+	public void LoadLevel(string levelToLoad)
+    {
+        
+    }
+}
